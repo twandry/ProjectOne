165cdc58605332961598693ab70ad7f18048d7eb
curnowbr <curnowbr@gmail.com>
2018-11-07 16:53:17 -0700

    Structure

---
+++ ProjectOne/ProjectOne/Controllers/MissionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectOne.Models;
+
+namespace ProjectOne.Controllers
+{
+    public class MissionController : Controller
+    {
+        // GET: Mission
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjectOne/ProjectOne/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectOne.Models
+{
+    public class User
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string subject { get; set; }
+        public string message { get; set; }
+
+    }
+}
