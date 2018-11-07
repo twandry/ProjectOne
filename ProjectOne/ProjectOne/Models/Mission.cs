6dfa08602c09b7713d3501aa06adeedef36e498b
curnowbr <curnowbr@gmail.com>
2018-11-07 16:38:43 -0700

    model

---
+++ ProjectOne/ProjectOne/Models/Mission.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectOne.Models
+{
+    public class Mission
+    {
+        public string name { get; set; }
+        public string presName { get; set; }
+        public string address { get; set; }
+        public string language { get; set; }
+        public string climate { get; set; }
+        public string domReligion { get; set; }
+        
+    }
+}
