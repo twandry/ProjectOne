b4263f3927b5574fd21264251e5b37d70b4ff172
meganhitchcock <megrhitch@gmail.com>
2018-11-07 17:08:07 -0700

    no change

--- ProjectOne/ProjectOne/Controllers/HomeController.cs
+++ ProjectOne/ProjectOne/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectOne.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -23,6 +24,7 @@ namespace ProjectOne.Controllers
         {
             return View();
         }
-    
+
+
     }
 }
